7a6c366e564ffa0e4d2b02a65f4ce0e8a972a616
Keshav <keshavtomar999@gmail.com>
2024-07-02 23:36:42 +0530

    my first animated chracter

---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+    <group position-y={-1}>
+    <Avatar />
+    </group>
+       
+        <ambientLight intensity={2} />
+        
+    </>
+  );
+};
