3fc26a0d6d66efc1cc1d3896f5f2f90d99cb66fd
Keshav <keshavtomar999@gmail.com>
2024-07-05 00:16:57 +0530

    Added office scene

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
+import { Office } from "./Office";
 
 
 export const Experience = () => {
@@ -13,10 +14,11 @@ export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <Sky />
-      <Environment preset="sunset" />
+      {/* <Sky />
+      <Environment preset="sunset" /> */}
     <group position-y={-1}>
-      <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
+      <ambientLight intensity={1} />
+      {/* <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
     <Avatar animation={animation} />
 
     {animation === "Typing" && (
@@ -32,7 +34,9 @@ export const Experience = () => {
     <mesh scale={5} rotation-x={-Math.PI*0.5} position-y={-0.001}>
       <planeGeometry />
       <meshStandardMaterial color="white" />
-    </mesh>
+    </mesh> */}
+
+    <Office />
 
     </group>
        
